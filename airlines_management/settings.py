97682266a8f5e6920082caf5e5cba8eec6b98fb0
Om-Kapse <kapseom378@gmail.com>
2025-09-07 18:15:31 +0530

    Add base template and update templates to use it
    Introduced a shared base.html template for consistent layout and navigation. Updated all relevant app templates to extend from base.html and use content blocks. Modified settings.py to include the project-level templates directory. Added requirements.txt for dependency management.

--- airlines_management/settings.py
+++ airlines_management/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'airlines_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User' #By us
+
